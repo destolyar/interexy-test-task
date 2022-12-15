8c69c4e83a3cda5789f23b22208eed384d3ca6aa
destolyar <dead.stolyar@gmail.com>
2022-12-15 23:59:01 +0400

    Header

---
+++ src/components/Layout/UserIconSVG.tsx
@@ -0,0 +1,12 @@
+export const UserIconSVG = () => {
+  return (
+    <svg width={32} height={32} enable-background="new 0 0 32 32" id="Stock_cut" version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+      <desc />
+      <g>
+        <circle cx="16" cy="16" fill="none" r="15" stroke="#000000" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2" />
+        <path d="M26,27L26,27   c0-5.523-4.477-10-10-10h0c-5.523,0-10,4.477-10,10v0" fill="none" stroke="#000000" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2" />
+        <circle cx="16" cy="11" fill="none" r="6" stroke="#000000" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2" />
+      </g>
+    </svg>
+  )
+}
